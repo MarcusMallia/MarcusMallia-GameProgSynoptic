9d07ae839634bb63543d8f1c5f6d7ac78367aec8
MarcusMallia <marcusjohnmallia@gmail.com>
2024-05-17 01:14:54 +0200

    Implement state machine for main menu, time trial, and versus mode
    - Developed a state machine to manage transitions between the main menu, time trial mode, and versus mode.

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -4,32 +4,86 @@ using UnityEngine.SceneManagement;
 public class GameStateManager : MonoBehaviour
 {
     public GameObject instructionsPanel;
-    public GameObject TimeTrial;
+
     public GameObject StartScene;
+    public GameObject TimeTrial;
     public GameObject Versus;
+
+    public GameObject Boat;
+
+    public Animator animator;
     
+    public static GameStateManager Instance { get; private set; }
+    public GameState CurrentState { get; private set; }
+
     void Start()
     {
           StartScene.SetActive(true);
+          ChangeState(GameState.MainMenu);
         // Hide instructions panel on start
         if (instructionsPanel != null)
         {
             instructionsPanel.SetActive(false);
         }
     }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            animator = GetComponent<Animator>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        CurrentState = newState;
+        HandleStateChange(newState);
+    }
     
+    private void HandleStateChange(GameState newState)
+    {
+        switch (newState)
+        {
+            case GameState.MainMenu:
+                animator.SetTrigger("MainMenu");
+                StartScene.SetActive(true);
+                Versus.SetActive(false);
+                TimeTrial.SetActive(false);
+                break;
+            case GameState.Versus:
+                animator.SetTrigger("LoadVersus");
+                Versus.SetActive(true);        
+                StartScene.SetActive(false);
+                TimeTrial.SetActive(false);
+                break;
+            case GameState.TimeTrial:
+                animator.SetTrigger("TimeTrial");
+                TimeTrial.SetActive(true);
+                StartScene.SetActive(false);
+                Versus.SetActive(false);
+                break;
+            // case GameState.GameOver:
+            //     animator.SetTrigger("GameOver");
+            //     break;
+        }
+    }
     // Loads time trial mode
     public void LoadTimeTrial()
     { 
-        TimeTrial.SetActive(true);
-
-        StartScene.SetActive(false);
-        Versus.SetActive(false);
+        ChangeState(GameState.TimeTrial);
     }
 
-    // Loads time versus mode
+    // Loads versus mode
     public void LoadVersus()
     {
+         ChangeState(GameState.Versus);
         Versus.SetActive(true);
         
         StartScene.SetActive(false);
